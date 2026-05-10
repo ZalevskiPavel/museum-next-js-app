5f4e4556571eda2b2b91fc86c05415011512b671
Zalevski Pavel <zalevskiipavel112@gmail.com>
2026-05-10 22:27:55 +0300

    added adaptation on all pages

--- components/NavBar/CustomNavBar.jsx
+++ components/NavBar/CustomNavBar.jsx
@@ -1,7 +1,29 @@
+'use client'
 import styles from "./nav-bar-styles.module.css"
 import NavData from "../../utils/nav-data.json"
+import { useState, useEffect } from "react";
 
 export default function CustomNavBar() {
+    const [burgerOn, setBurgerOn] = useState(false);
+
+    const toggleBurger = () => {
+        setBurgerOn(prev => !prev);
+        console.log(burgerOn);
+    }
+
+    useEffect(() => {
+        const handleResize = () => {
+            const width = window.innerWidth;
+            if (width > 1200) {
+                setBurgerOn(false);
+                console.log("yeeeeeeeeeee");
+               
+            }
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <nav className={styles.nav}>
             <ul>
@@ -24,45 +46,31 @@ export default function CustomNavBar() {
                     </li>
                 ))}
             </ul>
-            {/* <!--burger-mm-yammy-->
-                <div class="burger_nav">
-                    <div class="logo">
-                        <img src="src/Logo.png" alt="">
-                        <div>UXMCS MUSEUM</div>
-                    </div>
-                    <div class="menu_button">
-                        <div class="burger_menu">
-                            <div class="bar"></div>
-                            <div class="bar"></div>
-                            <div class="bar"></div>
-                        </div>
-                        <p>Menu</p>
-                    </div>
+            {/* <!--burger-mm-yummy-->*/}
+            <div className={styles.burger_nav}>
+                <div className={styles.logo}>
+                    <img src={NavData.logo.image} alt="" />
+                    <div>UXMCS MUSEUM</div>
                 </div>
-                <div class="hidden_nav">
-                    <div class="grid_container">
-                        <a href="">
-                            <p>Home</p>
-                            <div class="arrow">></div>
-                        </a>
-                        <a href="exhibitions_and_events.html">
-                            <p>Exhibition and Events</p>
-                            <div class="arrow">></div>
-                        </a>
-                        <!--<a href="">
-                            <p>Products</p>
-                            <div class="arrow">></div>
-                        </a>-->
-                        <a href="history.html">
-                            <p>History</p>
-                            <div class="arrow">></div>
-                        </a>
-                        <a href="contacts.html">
-                            <p>Contact</p>
-                            <div class="arrow">></div>
-                        </a>
+                <div className={styles.menu_button} onClick={toggleBurger}>
+                    <div className={styles.burger_menu}>
+                        {[styles.first_bar, styles.second_bar, styles.third_bar].map((item, index) => (
+                            <div key={index} className={`${styles.bar} ${burgerOn ? item : ''}`}></div>
+                        ))}
                     </div>
-                </div> */}
+                    <p>Menu</p>
+                </div>
+            </div>
+            <div className={`${styles.hidden_nav} ${burgerOn ? styles.open : ''}`}>
+                <div className={`${styles.grid_container} ${burgerOn ? styles.displ : ''}`}>
+                    {NavData.navigation.map((item) => (
+                        <a href={item.link} key={item.title}>
+                            <p>{item.title}</p>
+                            <div className={styles.arrow}>&gt;</div>
+                        </a>
+                    ))}
+                </div>
+            </div>
         </nav>
     );
 }
